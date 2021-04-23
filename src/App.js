c7c2be6582b2bc8995a8c2731ed4905dfbbf071d
Rares <rares_lungescu@yahoo.com>
2021-04-23 15:38:16 +0300

    test changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>Learn react</p>
 				<p>
-					Edit <code>src/App.js</code> and save to reload.
+					Edit <code>src/App.js</code> and save.
 				</p>
 				<a
 					className='App-link'
